c865d68b05e05201c192a6577854812fcd8d95f1
adam <adam.crossland@zero1.co.uk>
2018-10-17 16:29:16 +0100

    adding support for production as CDN

---
+++ MagentoCatalog/Model/Product/Image.php
@@ -0,0 +1,49 @@
+<?php
+namespace Mdoq\Connector\MagentoCatalog\Model\Product;
+
+class Image extends \Magento\Catalog\Model\Product\Image
+{
+	protected function isEnvironmentMdoq()
+	{
+		return (isset($_SERVER['SERVER_NAME']) && preg_match('/\.mdoq\.io$/', $_SERVER['SERVER_NAME']) === 1);
+	}
+	
+	/**
+     * First check this file on FS
+     * If it doesn't exist - try to download it from DB
+     *
+     * @param string $filename
+     * @return bool
+     */
+    protected function _fileExists($filename)
+    {
+    	//TODO change this to check remotely, then fallback to local
+    	if($this->isEnvironmentMdoq()){
+    		return true;
+    	}
+    	return parent::_fileExists($filename);
+    }
+    
+     /**
+     * @return $this
+     */
+    public function saveFile()
+    {
+    	if($this->isEnvironmentMdoq()){
+    		return $this;
+    	}
+    	return parent::saveFile();
+    }
+    
+    /**
+     * @see \Magento\Framework\Image\Adapter\AbstractAdapter
+     * @return $this
+     */
+    public function resize()
+    {
+    	if($this->isEnvironmentMdoq()){
+    		return $this;
+    	}
+    	return parent::saveFile();
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # magento2-Connector
 
+## Changelog
+
+1.2.0 -> 1.3.0
+- added initial 'Production as CDN' support
