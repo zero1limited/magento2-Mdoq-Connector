ed216a51e9463e704cc22e6bc72aa1169b98a531
adamzero1 <adam.crossland@zero1.co.uk>
2025-08-05 08:57:22 +0000

    fix: new version of Magento

--- Model/Config/Backend/AuthKey.php
+++ Model/Config/Backend/AuthKey.php
@@ -22,9 +22,9 @@ class AuthKey extends \Magento\Framework\App\Config\Value implements \Magento\Fr
 		\Magento\Framework\Registry $registry,
 		\Magento\Framework\App\Config\ScopeConfigInterface $config,
 		\Magento\Framework\App\Cache\TypeListInterface $cacheTypeList,
-		\Magento\Framework\Model\ResourceModel\AbstractResource $resource = null,
-		\Magento\Framework\Data\Collection\AbstractDb $resourceCollection = null,
 		\Magento\Framework\ObjectManagerInterface $objectManagerInterface,
+		?\Magento\Framework\Model\ResourceModel\AbstractResource $resource = null,
+		?\Magento\Framework\Data\Collection\AbstractDb $resourceCollection = null,
 		array $data = []
 	) {
 		parent::__construct($context, $registry, $config, $cacheTypeList, $resource, $resourceCollection, $data);
--- Model/Config/Backend/UrlKey.php
+++ Model/Config/Backend/UrlKey.php
@@ -35,10 +35,10 @@ class UrlKey extends \Magento\Framework\App\Config\Value implements \Magento\Fra
 		\Magento\Framework\Registry $registry,
 		\Magento\Framework\App\Config\ScopeConfigInterface $config,
 		\Magento\Framework\App\Cache\TypeListInterface $cacheTypeList,
-		\Magento\Framework\Model\ResourceModel\AbstractResource $resource = null,
-		\Magento\Framework\Data\Collection\AbstractDb $resourceCollection = null,
 		\Magento\Framework\ObjectManagerInterface $objectManagerInterface,
         \Magento\Store\Model\StoreManagerInterface $storeManagerInterface,
+		?\Magento\Framework\Model\ResourceModel\AbstractResource $resource = null,
+		?\Magento\Framework\Data\Collection\AbstractDb $resourceCollection = null,
 		array $data = []
 	) {
 		parent::__construct($context, $registry, $config, $cacheTypeList, $resource, $resourceCollection, $data);
